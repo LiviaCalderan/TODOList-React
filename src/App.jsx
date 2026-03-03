e7c3aeedfeb64bf20b28a80cf1b50646723cea6b
Lívia Calderan <liv_calderan@hotmail.com>
2026-03-03 10:16:30 -0400

    feat(app): background initial adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Todo from './components/Todo'
 
 const App = () => {
   return (
-    <div>
+    <div className='flex justify-center items-center min-h-screen' style={{
+    backgroundColor: '#fefef9',
+    backgroundImage: 'radial-gradient(circle, #c8c8b8 1px, transparent 1px)',
+    backgroundSize: '24px 24px',
+  }}>
       <Todo />
     </div>
   )
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Todo = () => {
   return (
-    <div>
+    <div className='bg-white w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-2xl'>
         <h1>My Todo List</h1>
     </div>
   )
